b8319122aef10ac08d367ebba69fea7d469f1a07
= <=>
2020-06-05 12:46:30 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime agora = DateTime.Now;
+            Console.WriteLine(agora);
+        }
+    }
+}
